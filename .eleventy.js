99707aab6f7ce4d0932feba4bdbe736a8c2f65a6
William Svoboda <wsvoboda@princeton.edu>
2021-09-14 18:47:59 -0400

    Generate open graph previews and optimized images only in production

--- .eleventy.js
+++ .eleventy.js
@@ -10,34 +10,36 @@ const markdownItAnchor = require("markdown-it-anchor");
 // Image shortcode using eleventy-img
 // https://www.brycewray.com/posts/2021/04/using-eleventys-official-image-plugin/
 async function imageShortcode(src, alt) {
-  let sizes = "(min-width: 1024px) 100vw, 50vw";
-  let srcPrefix = `./src/static/images/`;
-  src = srcPrefix + src;
-  console.log(`Generating image(s) from:  ${src}`);
-  if (alt === undefined) {
-    // Throw an error on missing alt (alt="" works okay)
-    throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
-  }
-  let metadata = await Image(src, {
-    widths: [600, 900, 1500],
-    formats: ["webp", "jpeg"],
-    urlPath: "/static/images/",
-    outputDir: "./build/static/images/",
-    /* =====
+  // Generate optimized images only in production
+  if (process.env.ELEVENTY_ENV === "production") {
+    let sizes = "(min-width: 1024px) 100vw, 50vw";
+    let srcPrefix = `./src/static/images/`;
+    src = srcPrefix + src;
+    console.log(`Generating image(s) from:  ${src}`);
+    if (alt === undefined) {
+      // Throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
+    }
+    let metadata = await Image(src, {
+      widths: [600, 900, 1500],
+      formats: ["webp", "jpeg"],
+      urlPath: "/static/images/",
+      outputDir: "./build/static/images/",
+      /* =====
       Now we'll make sure each resulting file's name will
       make sense to you. **This** is why you need
       that `path` statement mentioned earlier.
       ===== */
-    filenameFormat: function (id, src, width, format, options) {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-      return `${name}-${width}w.${format}`;
-    },
-  });
-  let lowsrc = metadata.jpeg[0];
-  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
-  return `<figure>
-            <picture>
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src);
+        const name = path.basename(src, extension);
+        return `${name}-${width}w.${format}`;
+      },
+    });
+    let lowsrc = metadata.jpeg[0];
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+    return `<figure>
+              <picture>
                 ${Object.values(metadata)
                   .map((imageFormat) => {
                     return `  <source type="${
@@ -54,9 +56,22 @@ async function imageShortcode(src, alt) {
                     alt="${alt}"
                     loading="lazy"
                     decoding="async">
-            </picture>
-            <figcaption>${alt}</figcaption>
-        </figure>`;
+              </picture>
+              <figcaption>${alt}</figcaption>
+            </figure>`;
+  } else {
+    urlPath = "/static/images/" + src;
+    return `<figure>
+              <picture>
+                <img
+                    src="${urlPath}"
+                    alt="${alt}"
+                    loading="lazy"
+                    decoding="async">
+              </picture>
+              <figcaption>${alt}</figcaption>
+            </figure>`;
+  }
 }
 
 module.exports = function (eleventyConfig) {
@@ -67,29 +82,34 @@ module.exports = function (eleventyConfig) {
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
 
-  // Generate social preview images
+  // Generate social preview images in production
   // https://bnijenhuis.nl/notes/2021-05-10-automatically-generate-open-graph-images-in-eleventy/
   eleventyConfig.on("afterBuild", () => {
-    const socialPreviewImagesDir = "build/static/images/social/";
-    fs.readdir(socialPreviewImagesDir, function (err, files) {
-      if (files.length > 0) {
-        files.forEach(function (filename) {
-          if (filename.endsWith(".svg")) {
-            let imageUrl = socialPreviewImagesDir + filename;
-            console.log(`Generating image(s) from:  ${imageUrl}`);
-            Image(imageUrl, {
-              formats: ["jpeg"],
-              outputDir: "./" + socialPreviewImagesDir,
-              filenameFormat: function (id, src, width, format, options) {
-                let outputFilename = filename.substring(0, filename.length - 4);
+    if (process.env.ELEVENTY_ENV === "production") {
+      const socialPreviewImagesDir = "build/static/images/social/";
+      fs.readdir(socialPreviewImagesDir, function (err, files) {
+        if (files.length > 0) {
+          files.forEach(function (filename) {
+            if (filename.endsWith(".svg")) {
+              let imageUrl = socialPreviewImagesDir + filename;
+              console.log(`Generating image(s) from:  ${imageUrl}`);
+              Image(imageUrl, {
+                formats: ["jpeg"],
+                outputDir: "./" + socialPreviewImagesDir,
+                filenameFormat: function (id, src, width, format, options) {
+                  let outputFilename = filename.substring(
+                    0,
+                    filename.length - 4
+                  );
 
-                return `${outputFilename}.${format}`;
-              },
-            });
-          }
-        });
-      }
-    });
+                  return `${outputFilename}.${format}`;
+                },
+              });
+            }
+          });
+        }
+      });
+    }
   });
 
   // Register image shortcode
